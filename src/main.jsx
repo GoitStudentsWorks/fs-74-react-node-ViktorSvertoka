b11b3fb529f4c31fb821f374b0287d3e87b080c6
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-21 11:02:22 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/power-pulse">
+        <BrowserRouter basename="/fs-74-react-node-ViktorSvertoka">
           <GlobalStyle />
           <App />
           <ToastContainer />
